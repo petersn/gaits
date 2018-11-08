a76c98657afc00052b090e0fb36c50626210810c
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2018-11-08 00:15:03 -0800

    Initial version that can do the full AlphaZero loop.

---
+++ generate_games.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import json, logging, base64
+import numpy as np
+import mcts
+import game
+
+MAX_STEP_RATIO = 10
+
+def generate_game(args):
+	game_engine = game.build_demo_game_engine()
+
+	entry = {"samples": [], "outcome": None}
+	m = mcts.MCTS(game_engine.initial_state)
+	all_steps = 0
+	while True:
+		if m.root_node.state.is_game_over():
+			break
+		most_visits = 0
+		while most_visits < args.visits and m.root_node.all_edge_visits < args.visits * MAX_STEP_RATIO:
+			all_steps += 1
+			edge = m.step()
+			most_visits = max(most_visits, edge.edge_visits)
+
+		# Compute the proportion of visits each move received.
+		total_visits = float(m.root_node.all_edge_visits)
+		weighted_moves = {
+			move: m.root_node.outgoing_edges[move].edge_visits / total_visits
+			for move in m.root_node.state.moves
+		}
+
+		# Mix the policies by their visit counts to get a training policy.
+		training_policy = np.sum([
+			weight * m.root_node.state.moves[move].policy
+			for move, weight in weighted_moves.iteritems()
+		], axis=0)
+
+		# Store training sample.
+		entry["samples"].append((
+			map(float, m.root_node.state.sensor_data),
+			map(float, training_policy),
+			float(m.root_node.visit_weighted_edge_score()),
+		))
+
+		# Step using the most visited move (with no noise).
+		selected_move = mcts.sample_by_weight(weighted_moves)
+		m.play(selected_move)
+
+	entry["outcome"] = m.root_node.state.compute_utility()
+
+	return entry
+
+if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+	)
+	parser.add_argument("--network", metavar="PATH", default="", help="Path to the model to load.")
+	parser.add_argument("--output", metavar="PATH", type=str, default=None, help="Path to write .json games to. Writes in append mode, so it won't overwrite existing games.")
+	parser.add_argument("--visits", metavar="N", default=50, type=int, help="When generating moves for games perform MCTS steps until the PV move has at least N visits.")
+
+	args = parser.parse_args()
+
+	mcts.initialize_model(args.network)
+
+	with open(args.output, "a") as f:
+		games_generated = 0
+		while True:
+			entry = generate_game(args)
+			s = json.dumps(entry)
+			s = base64.b64encode(s.encode("bz2"))
+			assert "\n" not in s
+			f.write(s)
+			f.write("\n")
+			f.flush()
+			games_generated += 1
+			print "Games generated:", games_generated, "utility =", entry["outcome"]
+
---
+++ looper.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+
+import os, glob, signal, subprocess, socket, atexit, time
+
+def count_games(paths):
+	total_games = 0
+	for path in paths:
+		with open(path) as f:
+			for line in f:
+				if line.strip():
+					total_games += 1
+	return total_games
+
+def kill(proc):
+	print "Killing:", proc
+	try:
+		os.kill(proc.pid, signal.SIGTERM)
+	except Exception, e:
+		print "ERROR in kill:", e
+	proc.kill()
+
+def generate_games(model_number):
+	# Touch the games file to initialize it empty if it doesn't exist.
+	for path in index_to_games_paths(model_number):
+		open(path, "a").close()
+
+	# Before we even launch check if we have enough games.
+	if count_games(index_to_games_paths(model_number)) >= args.game_count:
+		print "Enough games to start with!"
+		return
+
+	# Launch the games generation.
+	games_processes = [
+		subprocess.Popen([
+			"python", "generate_games.py",
+				"--network", index_to_model_path(model_number),
+				"--output", path,
+				"--visits", str(args.visits),
+		], close_fds=True)
+		for path in index_to_games_paths(model_number)
+	]
+	# If our process dies take the games generation down with us.
+	def _(games_processes):
+		def handler():
+			for proc in games_processes:
+				kill(proc)
+		atexit.register(handler)
+	_(games_processes)
+
+	# We now periodically check up on how many games we have.
+	while True:
+		game_count = count_games(index_to_games_paths(model_number))
+		print "Game count:", game_count
+		time.sleep(10)
+		if game_count >= args.game_count:
+			break
+
+	# Signal the process to die gracefully.
+	for proc in games_processes:
+		os.kill(proc.pid, signal.SIGTERM)
+	# Wait up to two seconds, then forcefully kill it.
+	time.sleep(2)
+	for proc in games_processes:
+		kill(proc)
+	print "Exiting."
+
+def index_to_model_path(i):
+	return os.path.join(args.prefix, "models", "model-%03i.nn" % i)
+
+def index_to_games_paths(i):
+	return [
+		os.path.join(args.prefix, "games", "model-%03i-%i.json" % (i, process_index))
+		for process_index in xrange(args.parallel_games_processes)
+	]
+
+if __name__ == "__main__":
+	import argparse
+
+	class Formatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+		pass
+
+	parser = argparse.ArgumentParser(
+		description="""
+Performs the main loop of alternating game generation (via generate_games.py)
+with training (via train.py). You specify a prefix path which will have the structure:
+  PREFIX/
+    games/
+      model-001.json
+      model-002.json
+      ...
+    models/
+      model-001.nn
+      model-002.nn
+      ...
+You are expected to create the PREFIX/games/ and PREFIX/models/ directories, and populate
+the initial PREFIX/models/model-001.nn file. Then looper.py will run in a main loop of:
+
+  1) Generate self-play games with the highest numbered present
+     model until reaching the minimum game count.
+  2) Train model n+1 from the current highest numbered model,
+     and a pool of games from recent iterations.
+
+It is relatively safe to interrupt and restart, as looper.py will automatically resume on
+the most recent model. (However, interrupting and restarting looper.py of course
+technically statistically biases the games slightly towards being shorter.)
+""",
+		formatter_class=Formatter,
+	)
+	parser.add_argument("--prefix", metavar="PATH", default=".", help="Prefix directory. Make sure this directory contains games/ and models/ subdirectories.")
+	parser.add_argument("--visits", metavar="N", type=int, default=50, help="At each move in the self-play games perform MCTS until the root node has N visits.")
+	parser.add_argument("--game-count", metavar="N", type=int, default=20, help="Minimum number of games to generate per iteration.")
+	parser.add_argument("--training-steps-const", metavar="N", type=int, default=200, help="Base number of training steps to perform per iteration.")
+	parser.add_argument("--training-steps-linear", metavar="N", type=int, default=50, help="We also apply an additional N steps for each additional iteration included in the training window.")
+	parser.add_argument("--training-window", metavar="N", type=int, default=10, help="When training include games from the past N iterations.")
+	parser.add_argument("--training-window-exclude", metavar="N", type=int, default=-1, help="To help things get started faster we exclude games from the very first N iterations from later training game windows.")
+	parser.add_argument("--parallel-games-processes", metavar="N", type=int, default=4, help="Number of games processes to run in parallel.")
+	args = parser.parse_args()
+	print "Arguments:", args
+
+	current_model_number = 1
+
+	while True:
+		start = time.time()
+		old_model = index_to_model_path(current_model_number)
+		new_model = index_to_model_path(current_model_number + 1)
+
+		if os.path.exists(new_model):
+			print "Model already exists, skipping:", new_model
+			current_model_number += 1
+			continue
+
+		print "=========================== Doing data generation for:", old_model
+		print "Start time:", start
+		generate_games(current_model_number)
+
+		print "=========================== Doing training:", old_model, "->", new_model
+		# Figure out the directories of games to train on.
+		low_index = min(current_model_number, max(args.training_window_exclude + 1, current_model_number - args.training_window + 1))
+		high_index = current_model_number
+		games_paths = sum((index_to_games_paths(i) for i in xrange(low_index, high_index + 1)), [])
+		
+		print "Game paths:", games_paths
+		steps = args.training_steps_const + args.training_steps_linear * (high_index - low_index + 1)
+		assert steps > 0
+		print "Steps:", steps
+		subprocess.check_call([
+			"python", "train.py",
+				"--steps", str(steps),
+				"--games"] + games_paths + [
+				"--old-path", old_model,
+				"--new-path", new_model,
+		], close_fds=True)
+
+		end = time.time()
+		print "Total seconds for iteration:", end - start
+		current_model_number += 1
+
